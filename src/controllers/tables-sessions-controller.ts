e6a51d8aeb13d68d11275d96fc1084738f5a9f46
devLarissaRodrigues <larissa.rodrigues@ccc.ufcg.edu.br>
2025-03-17 23:51:53 -0300

    Criando tabela de sessão da mesa

--- src/controllers/tables-sessions-controller.ts
+++ src/controllers/tables-sessions-controller.ts
@@ -10,3 +10,5 @@ class TablesSessionsController {
         }
     }
 }
+
+export { TablesSessionsController } 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express"
 
+import { tablesSessionsRoutes } from "./tables-sessions-routes"
 import { productsRouter } from "./products-routes"
 import { tablesRouter } from "./tables-routes"
 
 const routes = Router()
+routes.use("/tables-sessions", tablesSessionsRoutes)
 routes.use("/products", productsRouter)
 routes.use("/tables", tablesRouter)
 
